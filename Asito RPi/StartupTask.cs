fd48a266793e3284fcc25e679a5c40a3b590b75f
Sander <s.p.r.bloemen@student.utwente.nl>
2018-06-13 17:38:29 +0200

    API working

--- Asito RPi/StartupTask.cs
+++ Asito RPi/StartupTask.cs
@@ -4,22 +4,103 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
+using Windows.Networking.Sockets;
+using Windows.System.Threading;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Storage.Streams;
+using System.IO;
+using System.Diagnostics;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
-namespace Asito_RPi
+namespace AsitoRPi
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        public void Run(IBackgroundTaskInstance taskInstance)
+        private static BackgroundTaskDeferral _Deferral = null;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+            _Deferral = taskInstance.GetDeferral();
+
+            var webserver = new MyWebserver();
+
+            await ThreadPool.RunAsync(workItem =>
+            {
+                webserver.Start();
+            });
         }
     }
+
+    internal class MyWebserver
+    {
+        private const uint BufferSize = 8192;
+
+        public async void Start()
+        {
+            var listener = new StreamSocketListener();
+            var XmlHandler = new XmlHandler();
+            XmlHandler.MainAsync();
+
+            await listener.BindServiceNameAsync("8081");
+
+            listener.ConnectionReceived += async (sender, args) =>
+            {
+                var request = new StringBuilder();
+
+                using (var input = args.Socket.InputStream)
+                {
+                    var data = new byte[BufferSize];
+                    Windows.Storage.Streams.IBuffer buffer = data.AsBuffer();
+                    var dataRead = BufferSize;
+
+                    while (dataRead == BufferSize)
+                    {
+                        await input.ReadAsync(
+                             buffer, BufferSize, InputStreamOptions.Partial);
+                        request.Append(Encoding.UTF8.GetString(
+                                                      data, 0, data.Length));
+                        dataRead = buffer.Length;
+                    }
+                }
+
+                //string query = GetQuery(request);
+                string query = await XmlHandler.RunQuery(GetQuery(request));
+
+                using (var output = args.Socket.OutputStream)
+                {
+                    using (var response = output.AsStreamForWrite())
+                    {
+                        var html = Encoding.UTF8.GetBytes(
+                        $"<html><head><title>Background Message</title></head><body>{query}</body></html>");
+                        using (var bodyStream = new MemoryStream(html))
+                        {
+                            var header = $"HTTP/1.1 200 OK\r\nContent-Length: {bodyStream.Length}\r\nConnection: close\r\n\r\n";
+                            var headerArray = Encoding.UTF8.GetBytes(header);
+                            await response.WriteAsync(headerArray,
+                                                      0, headerArray.Length);
+                            await bodyStream.CopyToAsync(response);
+                            await response.FlushAsync();
+                        }
+                    }
+                }
+            };
+        }
+
+        private static string GetQuery(StringBuilder request)
+        {
+            var requestLines = request.ToString().Split(' ');
+
+            var url = requestLines.Length > 1
+                              ? requestLines[1] : string.Empty;
+
+            var uri = new Uri("http://localhost" + url);
+            var query = uri.Query;
+            Debug.WriteLine("Query is: " + query);
+            return query;
+            }
+
+    }
+
+
 }
---
+++ Asito RPi/XmlHandler.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Windows.Storage;
+
+namespace AsitoRPi
+{
+    class XmlHandler
+    {
+        public static async Task MainAsync()
+        {
+            var wfile = await ApplicationData.Current.LocalFolder.CreateFileAsync("Responses.xml");
+            Stream wstream = await wfile.OpenStreamForWriteAsync();
+
+            XDocument doc = new XDocument(new XElement("Objects"));
+
+            doc.Root.Add(new XElement("Object",
+                new XAttribute("ID", "00001"),
+                new XElement("Question", "Do you like your job?"),
+                new XElement("Responses", new XElement("Response", "Yes"), new XElement("Response", "No"), new XElement("Response", "Yes"))));
+                doc.Root.Add(new XElement("Object",
+                new XAttribute("ID", "00002"),
+                new XElement("Question", "Question 2"),
+                new XElement("Responses", new XElement("Response", "Yes"), new XElement("Response", "No"))));
+
+
+
+
+
+
+
+
+
+
+
+
+            doc.Save(wstream);
+            wstream.Close();
+
+
+
+        }
+
+        public async Task<String> RunQuery(String e)
+        {
+            var q = e.Split('/');
+
+            if (q.Count() == 1)
+            {
+              return await GetQuestionAsync(e);
+            }
+
+
+            if (q.Count() == 2)
+            {
+                String target = q[0].Remove(0, 1);
+                String ans = q[1];
+                WriteResponse(ans, target);
+                String res = "Done";
+                return res;
+            }
+
+            else
+            {
+                String res = "Error";
+                return res;
+            }
+            
+        }
+
+        public async Task<string> GetQuestionAsync(String e)
+        {
+
+            // var wfile = await ApplicationData.Current.LocalFolder.GetFileAsync("Responses.xml");
+            // Stream wstream = await wfile.OpenStreamForWriteAsync();
+            // XDocument wxmlFile = XDocument.Load(rstream);
+
+            e = e.Remove(0, 1);
+            Debug.WriteLine(e);
+
+            try
+            {
+                var file = await ApplicationData.Current.LocalFolder.GetFileAsync("Responses.xml");
+                Stream rstream = await file.OpenStreamForReadAsync();
+                var rxmlFile = XDocument.Load(rstream);
+                rstream.Close();
+
+                String xmlString = rxmlFile.Root.Elements("Object").Where(sel => sel.Attribute("ID").Value == e).FirstOrDefault().Element("Question").Value.ToString();
+                return xmlString;
+            }
+            
+                
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                String xmlString = "Not Found.";
+                return xmlString;
+            }
+                
+
+        }
+
+        public async void WriteResponse(String res, String targ)
+        {
+            var file = await ApplicationData.Current.LocalFolder.GetFileAsync("Responses.xml");
+            Stream rstream = await file.OpenStreamForReadAsync();
+            var rxmlFile = XDocument.Load(rstream);
+            rstream.Close();
+            Debug.WriteLine("Response: " + res);
+            Debug.WriteLine("Target: " + targ);
+
+            try
+            {
+                var add = new XElement("Response", res);
+                rxmlFile.Root.Elements("Object").Where(sel => sel.Attribute("ID").Value == targ).FirstOrDefault().Element("Responses").Add(add);
+                Stream wstream = await file.OpenStreamForWriteAsync();
+                rxmlFile.Save(wstream);
+                wstream.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
